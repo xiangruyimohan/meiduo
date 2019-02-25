3f11c4c10ce6b547528358255f990c2801329360
leolinzhentao <leolinzhentao@163.com>
2019-02-25 15:33:07 +0800

    林振涛

--- meiduo/meiduo/apps/orders/views.py
+++ meiduo/meiduo/apps/orders/views.py
@@ -1,13 +1,48 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from django_redis import get_redis_connection
 from decimal import Decimal
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 
 from goods.models import SKU
-from .serializers import OrderSettlementSerializer, CommitOrderSerializer
+from meiduo.utils.paginations import StandardResultsSetPagination
+from .models import OrderInfo, OrderGoods
+from .serializers import OrderSettlementSerializer, CommitOrderSerializer, Allordersserializer, Orderserializer
+
+
+class ListAllOrderGoodsView(ListAPIView):
+    permission_classes = [IsAuthenticated]  # 给视图指定权限
+    serializer_class = Allordersserializer  # 指定序列化器
+    pagination_class = StandardResultsSetPagination  # 指定分页类
+
+    def get_queryset(self):
+        user = self.request.user
+        User_orders = OrderInfo.objects.filter(user_id=user.id).order_by('-create_time')
+        return User_orders
+
+
+class UncommentgoodsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, order_id):
+        ordergoods = OrderGoods.objects.filter(order_id=order_id)
+        return Response(Orderserializer(ordergoods, many=True).data)
+
+
+class SavecommentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, order_id):
+        ordergood = OrderGoods.objects.get(order_id=request.data['order'], sku_id=request.data['sku'])
+        ordergood.score = request.data['score']
+        ordergood.is_anonymous = request.data['is_anonymous']
+        ordergood.comment = request.data['comment']
+        ordergood.is_commented = True
+        ordergood.save()
+        return Response("提交评论成功", status=status.HTTP_201_CREATED)
 
 
 # Create your views here.
