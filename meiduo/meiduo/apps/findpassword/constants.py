8b6b44a617ad5eef04eef41a2abf891ad37cc14d
AwonHy <awon9202@163.com>
2019-02-25 11:23:19 +0800

    创建找回密码模块

---
+++ meiduo/meiduo/apps/findpassword/constants.py
@@ -0,0 +1,7 @@
+IMAGE_CODE_REDIS_EXPIRES = 180  # 短信验证码有效期(单位:秒 )
+
+SMS_CODE_REDIS_EXPIRES = 300  # 短信验证码有效期(单位:秒 )
+
+SEND_SMS_CODE_INTERVAL = 60  # 短信标记有效期(单位:秒)
+
+SEND_SMS_TEMPLATE_ID = 1  # 短信模板
